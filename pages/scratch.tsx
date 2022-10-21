e9dfa40cc8b7a3b9edf5d33d9e2b1c381ee104bd
queq1890 <yuji.matsumoto@sun-asterisk.com>
2022-10-21 15:00:06 +0900

    Update scratch.tsx

--- pages/scratch.tsx
+++ pages/scratch.tsx
@@ -1,18 +1,26 @@
-import { TouchEventHandler } from 'react';
+import { TouchEventHandler, useState } from 'react';
 
 const Scratch = () => {
+  const [progress, setProgress] = useState(0);
+
   const handleTouchMove: TouchEventHandler<HTMLDivElement> = (event) => {
     console.log(event);
+    if (progress < 100) {
+      setProgress((current) => (current * 10 + 1) / 10);
+    }
   };
 
   return (
-    <div style={{ margin: 20 }}>
-      <div
-        style={{ background: 'red', width: 300, height: 300 }}
-        onTouchMove={handleTouchMove}
-      >
-        aaaaaaa
+    <div>
+      <div style={{ margin: 20 }}>
+        <div
+          style={{ background: 'lightblue', width: 300, height: 300 }}
+          onTouchMove={handleTouchMove}
+        >
+          aaaaaaa
+        </div>
       </div>
+      <div style={{ color: 'white' }}>progress: {progress}%</div>
     </div>
   );
 };
